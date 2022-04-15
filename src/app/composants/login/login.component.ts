4aba525971ed27823127411ab7f402a10c7f3406
orianeLalo <95689944+orianeLalo@users.noreply.github.com>
2022-04-15 14:12:15 +0200

    connexion ok

--- src/app/composants/login/login.component.ts
+++ src/app/composants/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username = "oriane";
+  password = "coucou";
+  message = false;
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
   login(loginForm:any){
-    
+    if (loginForm.value.username == this.username && loginForm.value.password == this.password){
+      this.router.navigate(["products"]);
+    } else {
+      this.message = true;
+    }
   }
 
 }
